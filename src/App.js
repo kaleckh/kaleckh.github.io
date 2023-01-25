e6d6fdf1215cd584e2c257fd7616779d1e6df6c6
Kaleck Hamm <kaleckh@gmail.com>
2023-01-24 22:29:10 -0700

    made socails bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
-import emailJs from 'emailjs-com'
+import {Twitter} from './media/Twitter'
+import {Linkedin} from './media/Linkedin'
+import {Insta} from './media/Insta'
+
 import mainPhoto from "./photos/mainPhoto.jpg";
 import photo1 from "./photos//photo1.JPG";
 import photo2 from "./photos/photo2.jpg";
@@ -7,10 +10,13 @@ import photo3 from "./photos/photo3.JPG";
 import photo4 from "./photos/photo4.JPG";
 import photo5 from "./photos/photo5.JPG";
 import photo6 from "./photos/photo6.JPG";
-import twitter from "./photos/twitter.jpg";
+
 import instagram from "./photos/instagram.jpg";
-import linkedin from "./photos/linkedin.jpg";
+
 import firstProject from "./photos/firstProject.png";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { Github } from "./media/Github";
 
 function App() {
   const images = document.querySelectorAll(".card");
@@ -27,19 +33,14 @@ function App() {
   images.forEach((image) => {
     observer.observe(image);
   });
+
+  const form = useRef();
+
+  
+    
   return (
     <div className="wholeScreen">
-      <div className="containerThree">
-        <div className="line"></div>
-        <div className="socials"> Check out my Socials </div>
-        <div className="line"></div>
-      </div>
-      <div className="socialsContainer">
-        <img className="twitter" src={twitter} alt="" />
-        <img className="twitter" src={instagram} alt="" />
-        <img className="twitter" src={linkedin} alt="" />
-      </div>
-      <header className="header sticky">
+      <header className="header ">
         <div className="headerWords">Kaleck</div>
         <div className="headerContainer">
           <div className="divWordContainer">
@@ -56,15 +57,28 @@ function App() {
         </div>
       </header>
       <div id="section-1" className="section one">
-        <div className="sectionOneFont">Hi! im Kaleck</div>
-        <div className="headerWord">
-          I'm a full stack Software Engineer with extensive experience in sales
-          and management
+        <div className="mainScreenLeft">
+          <div className="socialsContainer">
+            <a href="https://twitter.com/kalehamm"><Twitter/></a>
+            <a href="https://www.instagram.com/kale_hamm/?next=%2F"><Insta/></a>
+            <a href="https://www.linkedin.com/in/kaleck-hamm-692a54a1/"><Linkedin/></a>
+            <a href="https://github.com/kaleckh"><Github/></a>
+            
+            
+            <div className="line"></div>
+          </div>
         </div>
-        <div className="headerWord">React | Javascript | Node.js </div>
+        <div className="mainScreenRight">
+          <div className="sectionOneFont">Hi! im Kaleck</div>
+          <div className="headerWord">
+            I'm a full stack Software Engineer with extensive experience in
+            sales and management
+          </div>
+          <div className="headerWord">React | Javascript | Node.js </div>
+        </div>
       </div>
       <div id="section-2" className="redSection two">
-        <div className="twoTitle">
+        <div className="twoTitle sticky">
           <div className="photoTitle">I Like To Take Photos And Travel</div>
         </div>
         <div className="sectionTwoPhotoContainer">
@@ -95,7 +109,10 @@ function App() {
       </div>
 
       <div className="sectionThree three" id="section-4">
-        <div className="photoTitle contactTitle">Send me a message!</div>
+        <div className="photoTitle">Send me a message!</div>
+        <div className="smallText margin">
+          Always willing to do freelance work
+        </div>
         <div className="contactInputs">
           <div className="inputContainer">
             <div className="leftSide">
